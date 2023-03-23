636b73bc99808e4abf57de12725a0681dab57993
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2023-03-23 19:42:36 +0530

    Complete jQuery Module

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,10 @@
-$('h1').css('color', 'red');
+$('h1').addClass("big-title");
+
+$('button').click(function(){
+    if ($('h1').css("color") === "rgb(255, 255, 0)"){
+        $('h1').css("color", "purple");
+    }
+    else{
+        $('h1').css("color", "yellow");
+    }
+})
